bf7603f1d3c30919207fda9cc0a2bc081079aa64
aakash <aakashsolanki325@gmail.com>
2026-06-21 17:46:05 +0530

    updated api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:1606",
+  baseURL: "https://notes-app-backend-6sdw.onrender.com/",
   withCredentials: true,
 });
 
